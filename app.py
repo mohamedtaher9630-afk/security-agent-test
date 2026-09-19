1ec9d1e7e928bedc317745d9e9bcf88d3b0e2016
mohamedtaher9630-afk <mohamed.taher9630@gmail.com>
2026-09-19 06:52:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# ثغرة 1: كشف مفتاح API حساس بشكل مكشوف داخل الكود
 HARDCODED_API_KEY = "AIzaSyD-TEST-EXPOSED-KEY-998877665544"
 
 @app.route("/login", methods=["POST"])
@@ -14,7 +13,6 @@ def login():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
     
-    # ثغرة 2: SQL Injection - دمج مدخلات المستخدم مباشرة دون استخدام Parameterized Queries
     query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
     cursor.execute(query)
     
